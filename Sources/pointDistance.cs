1dee5ee20914241cb8250ac39cb2bbc4294d7d93
suguys <69029307+suguys@users.noreply.github.com>
2020-10-27 16:55:24 +0900

    Add files via upload

---
+++ Sources/pointDistance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+public class pointDistance : MonoBehaviour{
+
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject text;
+    void Update()
+    {
+        float distanceTo = pos1.transform.position.x - pos2.transform.position.x;
+        float valueChange = (1 - distanceTo) * 100; 
+        text.gameObject.GetComponent<Text>().text = valueChange.ToString();
+        Debug.Log(distanceTo);
+    }
+}

433bb92be902ba580ebea77ffcd5d8415b39f2f8
suguys <69029307+suguys@users.noreply.github.com>
2020-10-29 10:34:03 +0900

    Add files via upload

---
+++ Sources/AR/Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/TouchMgr.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchMgr : MonoBehaviour
+{
+    private Camera ARCam;
+    private Ray ray;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARCam = GameObject.Find("First Person Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ray = ARCam.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if(Physics.Raycast(ray, out hit, 100.0f, 1 << 8))
+            {
+               
+            }
+        }
+
+
+    }
+}
---
+++ Sources/AR/Assets/GoogleARCore/Examples/AugmentedFaces/Scripts/pointDistance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+public class pointDistance : MonoBehaviour
+{
+
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject text;
+    // Update is called once per frame
+
+    void Start()
+    {
+        // text.gameObject.GetComponent<Text>().text = "armyTest";
+    }
+    void Update()
+    {
+        float distanceTo = pos1.transform.position.x - pos2.transform.position.x;
+
+        float valueChange = (1 - distanceTo) * 100; 
+
+        text.gameObject.GetComponent<Text>().text = valueChange.ToString();
+        Debug.Log(distanceTo);
+    }
+
+}
